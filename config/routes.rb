373f46889c2d0c6d4eaf0763ade74507bff23e2d
yoko <youko.komuro@gmail.com>
2023-06-23 17:48:48 +0000

    rails kadai04 kadai 05

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+    
+  # PostsController
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+  
+  # TopicsController
+  get 'topics/new', to: 'topics#new', as: 'new_topics'
+  get 'topics/edit', to: 'topics#edit', as: 'edit_topics'
 end
